83bdd27c88f7acd73966c6b8fb860905c65bfc84
taner <tanerozer16@gmail.com>
2024-02-29 14:16:29 +0300

    DataContext added

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using CityGuideApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityGuideApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+    }
+}
