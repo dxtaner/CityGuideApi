c8811317fcd084453ed661239b1a8a45e0b6778f
taner <tanerozer16@gmail.com>
2024-03-01 16:43:56 +0300

    CityFor Dto added

---
+++ Dtos/CityForCreationDto.cs
@@ -0,0 +1,9 @@
+namespace CityGuideApi.Dtos
+{
+    public class CityForCreationDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int UserId { get; set; }
+    }
+}
